b0588264d1e810bfe82646cfd84ddfcb6a1ee00e
Rabina <32222576+rabinacathrine@users.noreply.github.com>
2020-03-24 16:38:13 +0530

    prograd

--- src/model/User.java
+++ src/model/User.java
@@ -1,9 +1,39 @@
 package model;
 import java.time.*;
-public class User{
-	public User() {
+	public class User{
 		String name;
-		int email;
-		LocalDate date;
+		String password;
+		LocalDateTime date;
+		String email;
+	 public User(String name, String password, LocalDateTime date)
+	 {
+		   this.name=name;
+		   this.password=password;
+		   this.date=date;
+
+	 }
+	 public void setEmail(String email)
+	 {
+		   this.email=email;
+	 }
+	 public void setPassword(String password)
+	 {
+		   this.password=password;
+	 }
+	 public void setDate(LocalDateTime date2)
+	 {
+	   this.date=date2;
+
+	 }
+
+	public char[] getEmail() 
+	{ char c[]=email.toCharArray();
+
+		return c;
 	}
+	public char[] getPassword() {
+		char c[]=password.toCharArray();
+		return c;
+	}
+
 }
